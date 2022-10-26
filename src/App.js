87a397e793182388baac932e3cf685caa7714b6b
Amjad-Ahmed <amjad.bscsses19@iba-suk.edu.pk>
2022-10-26 15:59:07 +0500

    concume Api and implement logic

--- src/App.js
+++ src/App.js
@@ -35,63 +35,6 @@ import React, { Component } from "react";
     var { isLoaded, items } = this.state;
     if (!isLoaded) {
       return <div>Loading...</div>;
-    } else {
-      return (
-        <div>
-          <form className="form-for-table-search">
-            SelectSymbol: &emsp;
-            <input
-              type="text"
-              value={this.state.search}
-              onChange={this.updateSearch.bind(this)}
-            />
-            &emsp; &emsp;{" "}
-            <button type="button" className="btn-submit">
-              Search
-            </button>
-            <br />
-            &emsp; Industry: &emsp; &emsp; &emsp;
-            <input
-              type="text"
-              value={this.state.search}
-              onChange={this.updateSearch.bind(this)}
-            />
-            <button type="button" className="btn-submit">
-              Search
-            </button>
-            &emsp; &emsp; History of Symbol and date : &emsp; &emsp; &emsp;
-            <input
-              type="text"
-              value={this.state.search}
-              onChange={this.updateSearch.bind(this)}
-            />
-            &emsp; &emsp;
-            <button type="button" className="btn-submit">
-              Search
-            </button>
-          </form>
-          <table border={2} cellPadding={1}>
-            <thead>
-              <tr>
-                <th>Symbol</th>
-                <th>Name</th>
-                <th>Industry</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {filteredItems.map((item) => (
-                <tr>
-                  <td key={item.symbol}>{item.symbol}</td>
-                  <td key={item.name}>{item.name}</td>
-                  <td key={item.industry}>{item.industry}</td>
-                </tr>
-              ))}
-              {"}"}
-            </tbody>
-          </table>
-        </div>
-      );
-    }
+    } 
   }
 }
